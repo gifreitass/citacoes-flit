5aaac7cd8f11452acf4f4fc7b0e2b87ed1a37fdd
Giovanna de Freitas <giovannadefreitasaraujo@hotmail.com>
2023-01-13 20:29:22 -0300

    mostrando as citações de acordo com a integração na api

--- citacoes/src/components/templates/QuotesList.tsx
+++ citacoes/src/components/templates/QuotesList.tsx
@@ -1,16 +1,50 @@
 import Quote from "../molecules/Quote"
 import Welcome from "../atoms/Welcome"
-import BestQuotes, { iBestQuotes } from "../atoms/BestQuotes"
+import BestQuotes, { iBestQuotes } from "../molecules/BestQuotes"
 import { useState } from "react"
+import axios from "axios";
+
+/**
+ * handle click -> fazer requisição -> definir resposta requisiçao no state -> passar state para componente Quote
+ */
+
+interface iGetQuoteReponse{
+    anime: string,
+    character: string,
+    quote: string
+}
 
 function QuotesList () {
     const [bestQuotes, setBestQuotes] = useState<iBestQuotes[]>([])
+    const [loadedQuote, setLoadedQuote] = useState<iGetQuoteReponse>()
+
+    const getQuote = async () => {
+        const response = await axios.get("https://animechan.vercel.app/api/random")
+        setLoadedQuote(response.data)
+    }
+
+    const handleClick = async () => {
+        await getQuote()
+    }
+
+    console.log(loadedQuote)
 
     return (
         <>
             <Welcome/> 
+            <button onClick={handleClick}>Iniciar votação</button>
             {/* showButton define a prop como true (se a prop não for passada é false) */}
-            <Quote showButton bestQuotes={bestQuotes} setBestQuotes={setBestQuotes} description='Pode se encontrar a felicidade mesmo nas horas mais sombrias, se a pessoa se lembrar de acender a luz.' movie='Harry Potter, Harry Potter e o Prisioneiro de Azkaban'/>
+            {loadedQuote &&
+                <Quote 
+                showButton 
+                bestQuotes={bestQuotes} 
+                setBestQuotes={setBestQuotes} 
+                description={loadedQuote.quote}
+                movie={loadedQuote.anime}
+                character={loadedQuote.character}
+                getQuote={getQuote}
+                />
+            }
             <BestQuotes bestQuotes={bestQuotes} />
         </>
     )
